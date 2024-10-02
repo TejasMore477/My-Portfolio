99bec335f2c8577068f653dd18ed7a309127e27f
Tejas More <tejasmore660@gmail.com>
2024-10-02 23:15:06 +0530

    added arrow

--- src/components/heroSection/Circle.jsx
+++ src/components/heroSection/Circle.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import { BsArrowUpRight } from "react-icons/bs";
 
 function Circle() {
   return (
     <motion.div 
-      className='size-[13rem] rounded-full bg-redO inline-block' 
+      className='size-[13rem] rounded-full bg-redO grid place-content-center ' 
       initial={{ scale: 0 }} 
       animate={{ scale: 1 }}  
-      transition={{ delay: 1, duration: 1, ease: [0.5, 1, 0.89, 1] }} 
-    />
+      transition={{ delay: 1, duration: 1, ease: [0.5, 1, 0.89, 1] }}
+    >
+        <span className='font-bold text-[6rem]'><BsArrowUpRight /></span> 
+    </motion.div>
   );
 }
 
