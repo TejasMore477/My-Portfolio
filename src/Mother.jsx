416038a1909dc88f63d8564f3befa85f9b6ca459
Tejas More <tejasmore660@gmail.com>
2024-10-06 20:18:25 +0530

    created letsdoit component

--- src/Mother.jsx
+++ src/Mother.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Navbar from './components/navbar/Navbar'
 import MenuPage from './components/menupage/MenuPage'
 import Landing from './components/heroSection/Landing'
+import LetsDo from './components/letsDoSection/LetsDo'
 
 function Mother({loaderAnime }) {
 
@@ -10,15 +11,13 @@ function Mother({loaderAnime }) {
       setShowMenu(prev => !prev)
   }
 
-  
-
   return (
   <>
-  
     <div className='bg-zinc-950 min-h-screen w-full text-white overflow-hidden'>
       <Navbar handelMenu={handelMenu}/>
       <MenuPage showMenu={showMenu} setShowMenu={setShowMenu} handelMenu={handelMenu}/>
       <Landing loaderAnime={loaderAnime} />
+      <LetsDo/>
     </div>
   </>
   )
