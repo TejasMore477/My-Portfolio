359ef009d7e9af213331d8548ac6e447ba18f656
Tejas More <tejasmore660@gmail.com>
2024-11-26 21:27:11 +0530

    made loader responsive

--- src/components/loaderPage/dum.jsx
+++ src/components/loaderPage/dum.jsx
@@ -25,22 +25,22 @@ function Loader({ setLoaderAnime, loaderAnime }) {
 
   return (
     <motion.div
-      className="w-full h-screen overflow-hidden bg-zinc-900  text-white font-Poppins grid place-content-center absolute z-30"
+      className="w-full h-screen overflow-hidden bg-zinc-900 sm:bg-yellow-900 md:bg-orange-800 lg:bg-lime-900 xl:bg-sky-950 text-white font-Poppins grid place-content-center absolute z-30"
       initial={{ y: 0, opacity: 1 }}
       animate={loaderAnime && { y: "-200%" }}
       transition={{ duration: 1, delay: 0.2, ease: easeInOut }}
     >
       <div className="md:flex items-center px-5 sm:px-0 w-full relative">
-        <img className=" md:size-[4.5rem] lg:size-20 sm:size-[12vw] size-[16.5vw]" src={ganpatiBappa} alt="Shri Ganesha" />
+        <img className=" md:size-[4.5rem] lg:size-20 sm:size-[12vw] size-[20vw]" src={ganpatiBappa} alt="Shri Ganesha" />
 
-        <h2 className="lg:text-4xl md:w-auto w-[90%] md:text-[2.9vw] sm:text-[5vw] text-3xl font-light px-4 leading-none">
+        <h2 className="lg:text-4xl md:w-full w-[90%] md:text-[2.9vw] sm:text-[5vw] text-3xl font-light px-4 leading-none">
           Tejas Madhukar More
         </h2>
 
         <motion.button
           type="button"
           onClick={() => setLoaderAnime((prev) => !prev)}
-          className="w-14 h-14 sm:w-12 sm:h-12 bottom-[2.2rem] left-[2rem] md:static absolute sm:ml-[10vw] ml-[6rem] md:ml-[0vw] border-solid border-[2px] border-redO rounded-full grid place-content-center"
+          className="w-14 h-14 sm:w-12 sm:h-12 bottom-[2.2rem] left-[2rem] sm:static absolute sm:ml-[10vw] ml-[6rem] md:ml-0 border-solid border-[2px] border-redO rounded-full grid place-content-center"
           initial={{ scale: 0, opacity: 0 }}
           animate={{ scale: 1, opacity: 1 }}
           transition={{ duration: 2, delay: 0.5 }}
@@ -55,7 +55,7 @@ function Loader({ setLoaderAnime, loaderAnime }) {
           </motion.span>
         </motion.button>
 
-        <h2 className="lg:text-4xl text-3xl md:text-[2.9vw] sm:text-[5vw] sm:pl-[17vw] md:pl-[1vw] font-light px-4  sm:leading-none mt-7 md:mt-0">
+        <h2 className="lg:text-4xl text-3xl md:text-[2.9vw] sm:text-[5vw] sm:pl-[17vw] md:pl-[1.2vw] font-light px-4  sm:leading-none mt-7 sm:mt-0">
           Web
           <motion.div
             className="inline-block ml-3 w-[12rem]"
