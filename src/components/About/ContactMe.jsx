307ddc4c9f65a2177553d464744b2d0c41e252f0
Tejas More <tejasmore660@gmail.com>
2024-12-10 17:52:17 +0530

    a to mail me

--- src/components/About/ContactMe.jsx
+++ src/components/About/ContactMe.jsx
@@ -4,10 +4,11 @@ import React from 'react'
 function ContactMe({paddingY = 10, text = "< Contact Me >", textPosition = "end" , width="full"}) {
   return (
     <div className={`w-${width} text-${textPosition}`}>
-        <motion.button 
+        <motion.a 
+        href='mailto:tejasmore660@gmail.com'
           whileHover={{ scale: 1.1}}
           transition={{ type: "spring", stiffness: 300 }}
-        className={`font-Poppins xl:text-[0.9vw] lg:text-[1.1vw] md:text-[1.4vw] sm:text-[1.9vw] text-[3.2vw] py-${paddingY} text-end text-redO`}>{text}</motion.button>
+        className={`font-Poppins xl:text-[0.9vw] lg:text-[1.1vw] md:text-[1.4vw] sm:text-[1.9vw] text-[3.2vw] py-${paddingY} text-end text-redO`}>{text}</motion.a>
     </div>
   )
 }
