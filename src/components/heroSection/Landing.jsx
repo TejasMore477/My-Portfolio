c47ccffecbab458918a3d1c6522c70b43cf95c3c
Tejas More <tejasmore660@gmail.com>
2024-12-11 12:05:10 +0530

    rounded-b-2xl

--- src/components/heroSection/Landing.jsx
+++ src/components/heroSection/Landing.jsx
@@ -5,7 +5,7 @@ import Border from "./Border";
 
 function Landing({ loaderAnime }) {
   return (
-    <div className="md:h-[105vh] h-[75vh] flex flex-col items-start justify-end xl:px-[4.5rem] sm:px-[2vw] bg-neutral-900 sm:pb-[5vh] pb-[3vh] ">
+    <div className="md:h-[105vh] h-[75vh] flex flex-col items-start justify-end xl:px-[4.5rem] sm:px-[2vw] bg-neutral-900 sm:pb-[5vh] pb-[3vh] rounded-b-2xl">
       <div className="pb-16 w-full px-[1rem] sm:px-0">
         <p className=" lg:text-[0.8vw] md:text-[1vw] sm:text-[1.3vw] text-[2.5vw] font-Orbitron font-light tracking-[0.2rem]">
           @WebDeveloper
